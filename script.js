404f9d1e14bca0bb48e638ace8a840af41980ced
Pedro Italo <pedroitl15@gmail.com>
2025-11-27 00:51:06 -0300

    add feed artist pages to start project

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 ReactDOM.render(
-    <div>
+    <div className="menu-suspenso">
         <div className="logo">
-            <img></img>
+            <a href=""><img src="/assets/logotipo-branco.png" alt=""></img></a>
         </div>
         <div className="busca">
             <input placeholder="busca"></input>
             <a href=""><i class="bi bi-search"></i></a>
         </div>
-        <div>
+        <div className="barra-tarefas">
             <a href=""><i class="bi bi-house"></i></a>
             <a href=""><i class="bi bi-briefcase"></i></a>
             <a href=""><i class="bi bi-bell"></i></a>
@@ -15,11 +15,9 @@ ReactDOM.render(
             <a href=""><i class="bi bi-person"></i></a>
         </div>
         <div className="logout">
-                
-                <a href=""><i class="bi bi-box-arrow-right"></i></a>
-                
+            <a href=""><i class="bi bi-box-arrow-right"></i></a>
         </div>
-        
     </div>,
     document.getElementById("menu-suspenso")
 );
+
